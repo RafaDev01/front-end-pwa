3498062b0f6f2f8ba4a21fcb5d871ed30e699049
Rafael do Nascimento Santos <rflwwe123@hotmail.com>
2025-02-25 23:49:24 -0300

    icons adicionados

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import PushNotificationManager from "../components/PushNotificationManager";
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(reg => console.log('Service Worker registrado:', reg))
+      .catch(err => console.error('Erro ao registrar SW:', err))
+  })
+}
+
 export default function Home() {
   return (
     <main style={{ padding: "20px", textAlign: "center" }}>
